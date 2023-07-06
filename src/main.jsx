7572f535e6c28e25546ebe6004819de8a1fa353a
yrohit8 <rohityadav8720@gmail.com>
2023-07-06 12:30:59 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "https://nodejs-todoapp.onrender.com/api/v1";
+export const server = "https://task-set-app.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
